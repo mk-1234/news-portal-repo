d91c2075d3b7559c3e84ee36de830a49ade237bf
MK <m.krznar@gmail.com>
2024-05-29 16:43:47 +0200

    "config variables added"

--- config.js
+++ config.js
@@ -6,17 +6,26 @@ module.exports = {
       user     : 'root',
       password : 'root',
       database : 'db_newsportal',
-      debug    :  false
+      debug    : false
     },*/
-    pool: {
+    /*pool: {
       connectionLimit : 100,
       host     : 'eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
       user     : 'g0arga8ctz031l48',
       password : 'mftaskya376b73p0',
       database : 'd4mtfzzkolfb74cm',
       port     : 3306,
-      debug    :  false
-    },
-    secret:'stringfortokenABcaBcEfhfddSfht3123dfsVVsdZr62'
+      debug    : false
+    }, 
+    secret:'stringfortokenABcaBcEfhfddSfht3123dfsVVsdZr62', */
+    pool: {
+      connectionLimit : process.env.CONNECTION_LIMIT || 100,
+      host     : process.env.HOST || 'localhost',
+      user     : process.env.USER || 'root',
+      password : process.env.PASSWORD || 'root',
+      database : process.env.DATABASE || 'db_newsportal',
+      debug    : process.env.DEBUG || false
+    }, 
+    secret: process.env.SECRET_STRING || 'justarandomstring'
   }
   
